e59381c71578ca6a6681bf45a7f15e7aa94b3aba
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-07-03 13:36:38 +0200

    nginx

--- proxy/compose/build/entrypoint.sh
+++ proxy/compose/build/entrypoint.sh
@@ -6,6 +6,9 @@
 #Arrancamos squid
 [ -f /etc/squid/squid.conf ] && /usr/sbin/squid -sYC
 
+#Arrancamos nginx
+[ -f /etc/nginx/nginx.conf ] && /usr/sbin/nginx -g 'daemon on; master_process on;'
+
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
 
