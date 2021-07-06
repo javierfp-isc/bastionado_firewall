344fbca9bfd43b628fddd9705b1e4d9b41c7ece9
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-07-06 18:15:26 +0200

    IDs suricata

--- ids/compose/build/entrypoint.sh
+++ ids/compose/build/entrypoint.sh
@@ -3,8 +3,18 @@
 #Si existe el archivo /etc/routes lo ejecuta
 [ -f /etc/routes ] && sh /etc/routes
 
-#Arrancamos snort
+#Arrancamos snort si existe el servicio
 [ -f /etc/snort/snort.conf ] && snort -A console -i eth1 -u snort -g snort -c /etc/snort/snort.conf
+
+#Arrancamos suricata si existe el servicio
+if [ -f /etc/suricata/suricata.yaml ]
+then
+	rm /var/run/suricata.pid
+	/usr/bin/suricata -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid --pcap -D
+	#/usr/bin/suricata -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid --af-packet -D
+	#Para el modo de captura nfqueue es necesario meter reglas iptables
+	#/usr/bin/suricata -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid -q 0 -D
+fi
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
