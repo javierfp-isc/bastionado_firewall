ed59e18aa78484f328f57f0e4f47487af280889c
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-08-28 19:54:59 +0200

    Cambio de imagen elk

--- elk/compose/build/entrypoint.sh
+++ elk/compose/build/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-#Uso exec para lanzar un proceso de arranque de systemd
-exec /lib/systemd/systemd
+#Si existe el archivo /etc/routes lo ejecuta
+[ -f /etc/routes ] && sh /etc/routes
+
+#Establece los servidores DNS a 8.8.8.8 y 8.8.4.4
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+
+#Lanza el proceso de arranque dentro de la imagen
+exec /usr/local/bin/start.sh
