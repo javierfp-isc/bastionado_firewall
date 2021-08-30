c70fe86662412d659c25da46495a7dac61add7ce
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-08-30 22:34:20 +0200

    Cambios en inicio elk

--- elk/compose/build/entrypoint.sh
+++ elk/compose/build/entrypoint.sh
@@ -8,4 +8,7 @@ echo "nameserver 8.8.8.8" > /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 
 #Lanza el proceso de arranque dentro de la imagen
-exec /usr/local/bin/start.sh
+/usr/local/bin/start.sh
+
+#Uso exec para lanzar un proceso independiente de bucle infinito
+exec bash -c "while true;do sleep 10;done"
