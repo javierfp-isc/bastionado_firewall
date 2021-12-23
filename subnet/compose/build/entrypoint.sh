ce3526603838e5b87fb5e013375dcbfea3b6c154
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-12-23 18:06:59 +0100

    Orden comandos

--- subnet/compose/build/entrypoint.sh
+++ subnet/compose/build/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#Si existe el archivo /etc/routes lo ejecuta
-[ -f /etc/routes ] && sh /etc/routes
-
 #Borramos la configuración previa de la red de docker
 ip addr flush eth0 && ip addr flush eth1
 
 #Recargamos la configuración de red utilizando el paquete ifupdown
 service networking restart
+
+#Si existe el archivo /etc/routes lo ejecuta
+[ -f /etc/routes ] && sh /etc/routes
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
