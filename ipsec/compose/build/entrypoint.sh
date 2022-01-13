da0dcdf5ac7fe33bebbe0fc6ddf73e334a7b7a41
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-01-13 17:59:16 +0100

    VPN site to site con 2 pfsense

--- ipsec/compose/build/entrypoint.sh
+++ ipsec/compose/build/entrypoint.sh
@@ -3,8 +3,8 @@
 #Si existe el archivo /etc/routes lo ejecuta
 [ -f /etc/routes ] && sh /etc/routes
 
-#Establece el servidor DNS al pfsense
-echo "nameserver 192.168.22.100" > /etc/resolv.conf
+#Establece el servidor DNS de google
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
